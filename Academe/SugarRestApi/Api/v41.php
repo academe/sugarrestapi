e0ce97b6aa1f229be1fda7953aaac91518fba2f9
Jason Judge <jason.judge@consil.co.uk>
2013-05-08 11:51:39 +0100

    Added V4_1 (v41) API class, albeit with not much in it yet.

---
+++ Academe/SugarRestApi/Api/v41.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * v4_1 of the SugarCRM API
+ * Ideally this class would be called v4_1, but PSR-0 treats underscores in a special way.
+ */
+
+namespace Academe\SugarRestApi\Api;
+
+class v41 extends v4
+{
+    public $version = '4_1';
+}
